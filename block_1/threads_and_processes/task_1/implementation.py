8de7b0884929b23e80e2fefdc8ce3c4d27da7637
e.kapkaev <e.kapkaev@bars.group>
2022-06-17 23:11:31 +0500

    задания по второму блоку

--- block_1/threads_and_processes/task_1/implementation.py
+++ block_1/threads_and_processes/task_1/implementation.py
@@ -1,25 +1,5 @@
-import multiprocessing
-
-import threading
-from threading import Thread
-
 class Composer:
-
-    front = multiprocessing.Process(target=self.aaa)
-    back = multiprocessing.Process()
-
-    def start(self):
-        self.front.start()
-        self.back.start()
-        self.front.join()
-        self.back.join()
-
-    def get_front(self):
-        return self.front
-
-    def aaa(self):
-        return 6
-
+    pass
 
 
 
