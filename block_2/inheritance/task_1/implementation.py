9b26c9defad182a5709fcd7b6f779e011f4ba1dd
e.kapkaev <e.kapkaev@bars.group>
2022-06-18 14:10:16 +0500

    задания по второму блоку

--- block_2/inheritance/task_1/implementation.py
+++ block_2/inheritance/task_1/implementation.py
@@ -1,6 +1,9 @@
 class Employee:
     """Сотрудник."""
     def __init__(self, salary):
+        self.dev = 0
+        self.analyse = 0
+        self.test = 0
         self.salary = salary
 
 class CEO(Employee):
@@ -14,34 +17,26 @@ class Developer(Employee):
     def __init__(self, salary):
         super().__init__(salary)
         self.dev = 1
-        self.analyse = 0
-        self.test = 0
 
 class Analyst(Employee):
     def __init__(self, salary):
         super().__init__(salary)
-        self.dev = 0
         self.analyse = 1
-        self.test = 0
 
 class Tester(Employee):
     def __init__(self, salary):
         super().__init__(salary)
-        self.dev = 0
-        self.analyse = 0
         self.test = 1
 
 class TeamLead(Employee):
     def __init__(self, salary):
         super().__init__(salary)
         self.dev = 1
-        self.analyse = 0
         self.test = 1
 
 class ProductOwner(Employee):
     def __init__(self, salary):
         super().__init__(salary)
-        self.dev = 0
         self.analyse = 1
         self.test = 1
 
@@ -55,87 +50,87 @@ class Freelancer():
 class Organization:
     """Организация."""
 
-    analyse = 0
-    dev = 0
-    test = 0
-    total_salary = 0
+    __analyse = 0
+    __dev = 0
+    __test = 0
+    __total_salary = 0
 
     @property
     def can_analyze_count(self):
         """Количество сотрудников, которые могут анализировать задачи."""
-        return self.analyse
+        return self.__analyse
 
     @property
     def can_develop_count(self):
         """Количество сотрудников, которые могут разрабатывать задачи."""
-        return self.dev
+        return self.__dev
 
     @property
     def can_test_count(self):
         """Количество сотрудников, которые могут тестировать задачи."""
-        return self.test
+        return self.__test
 
     def accept_employee(self, employee):
         """Принимает сотрудника на работу."""
         if not isinstance(employee, Employee):
             raise TypeError
         if isinstance(employee, CEO):
-            self.analyse += 1
-            self.dev += 1
-            self.test += 1
-            self.total_salary += employee.salary
+            self.__analyse += 1
+            self.__dev += 1
+            self.__test += 1
+            self.__total_salary += employee.salary
         elif isinstance(employee, Developer):
-            self.dev += 1
-            self.total_salary += employee.salary
+            self.__dev += 1
+            self.__total_salary += employee.salary
         elif isinstance(employee, Tester):
-            self.test += 1
-            self.total_salary += employee.salary
+            self.__test += 1
+            self.__total_salary += employee.salary
         elif isinstance(employee, Analyst):
-            self.analyse += 1
-            self.total_salary += employee.salary
+            self.__analyse += 1
+            self.__total_salary += employee.salary
         elif isinstance(employee, TeamLead):
-            self.dev += 1
-            self.test += 1
-            self.total_salary += employee.salary
+            self.__dev += 1
+            self.__test += 1
+            self.__total_salary += employee.salary
         elif isinstance(employee, ProductOwner):
-            self.analyse += 1
-            self.test += 1
-            self.total_salary += employee.salary
+            self.__analyse += 1
+            self.__test += 1
+            self.__total_salary += employee.salary
         return self
 
     def accept_employees(self, *args):
         """Принимает сотрудников на работу."""
         for employee in args:
             if isinstance(employee, CEO):
-                self.analyse += 1
-                self.dev += 1
-                self.test += 1
-                self.total_salary += employee.salary
+                self.__analyse += 1
+                self.__dev += 1
+                self.__test += 1
+                self.__total_salary += employee.salary
             elif isinstance(employee, Developer):
-                self.dev += 1
-                self.total_salary += employee.salary
+                self.__dev += 1
+                self.__total_salary += employee.salary
             elif isinstance(employee, Tester):
-                self.test += 1
-                self.total_salary += employee.salary
+                self.__test += 1
+                self.__total_salary += employee.salary
             elif isinstance(employee, Analyst):
-                self.analyse += 1
-                self.total_salary += employee.salary
+                self.__analyse += 1
+                self.__total_salary += employee.salary
             elif isinstance(employee, TeamLead):
-                self.dev += 1
-                self.test += 1
-                self.total_salary += employee.salary
+                self.__dev += 1
+                self.__test += 1
+                self.__total_salary += employee.salary
             elif isinstance(employee, ProductOwner):
-                self.analyse += 1
-                self.test += 1
-                self.total_salary += employee.salary
+                self.__analyse += 1
+                self.__test += 1
+                self.__total_salary += employee.salary
             elif isinstance(employee, ProductOwner):
-                self.analyse += 1
-                self.test += 1
-                self.total_salary += employee.salary
+                self.__analyse += 1
+                self.__test += 1
+                self.__total_salary += employee.salary
             elif isinstance(employee, Freelancer):
-                self.analyse += employee.analyse
-                self.test += employee.test
-                self.dev += employee.dev
+                self.__analyse += employee.analyse
+                self.__test += employee.test
+                self.__dev += employee.dev
 
     def calculate_salary(self):
         """Начисляет заработную плату сотрудникам.
@@ -143,7 +138,7 @@ class Organization:
         Returns:
             Возвращает общую сумму всех начислений
         """
-        return self.total_salary
+        return self.__total_salary
 
 
 
