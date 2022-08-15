69f88c1c1adb001514046ab724199839fe3f8d8b
e.kapkaev <e.kapkaev@bars.group>
2022-08-15 13:03:52 +0500

    индексы

--- block_9/indexes/task_1/models.py
+++ block_9/indexes/task_1/models.py
@@ -11,14 +11,14 @@ class Employee(models.Model):
     """Сотрудник."""
 
     inn = models.CharField('ИНН', max_length=12)
-    fname = models.CharField('Фамилия', max_length=300)
-    iname = models.CharField('Имя', max_length=300)
-    oname = models.CharField('Отчество', max_length=300, null=True)
-    country = models.CharField('Страна местонахождения на момент приема', max_length=3)
+    fname = models.CharField('Фамилия', max_length=300, db_index=True)
+    iname = models.CharField('Имя', max_length=300, db_index=True)
+    oname = models.CharField('Отчество', max_length=300, null=True, db_index=True)
+    country = models.CharField('Страна местонахождения на момент приема', max_length=3, db_index=True)
     department_id = models.IntegerField('ID подразделения')
     position_id = models.IntegerField('ID должности')
-    begin = models.DateField('Дата приема')
-    end = models.DateField('Дата увольнения', null=True)
+    begin = models.DateField('Дата приема', db_index=True)
+    end = models.DateField('Дата увольнения', null=True, db_index=True)
     additional_info = models.TextField('Дополнительная информация', default='', db_index=True)
 
     class Meta:
